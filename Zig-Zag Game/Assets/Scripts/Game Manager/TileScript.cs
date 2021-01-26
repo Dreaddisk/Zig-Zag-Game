c7a059ee8550ec160d6983df4288c333fd491d34
Jose <joey_manuel@outlook.com>
2021-01-26 17:26:46 -0500

    5. Tile Scripts
    Finished the TileScript.cs file

---
+++ Zig-Zag Game/Assets/Scripts/Game Manager/TileScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    #region Variables
+    private Rigidbody myBody;
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private GameObject gem;
+
+    [SerializeField]
+    private float chanceForCollectable;
+
+    #endregion
+
+    // Use this for initialization
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    private void Start()
+    {
+        if(Random.value < chanceForCollectable)
+        {
+            Vector3 temp = transform.position;
+            temp.y += 2f;
+            Instantiate(gem, temp, Quaternion.identity);
+        }
+    }
+
+    IEnumerator TriggerFallingDown()
+    {
+        yield return new WaitForSeconds(0.3f);
+        myBody.isKinematic = false;
+        audioSource.Play();
+        StartCoroutine(TurnOffGameObject());
+    }
+
+    IEnumerator TurnOffGameObject()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Ball")
+        {
+            StartCoroutine(TriggerFallingDown());
+        }
+    }
+}
