3bf0d0d7722917fcfb0d1a88622f9770014b797d
Jose <joey_manuel@outlook.com>
2021-01-26 09:41:49 -0500

    3. Ball controller
    Created the following files
    - BallScripts.cs
    - GameplayController.cs

---
+++ Zig-Zag Game/Assets/Scripts/Player Script/BallScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    #region Variable
+    private Rigidbody myBody;
+
+    private bool rollLeft;
+
+    public float speed = 4f;
+
+    #endregion Variable
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody>();
+        rollLeft = true;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+        CheckBallOutOfBounds();
+    }
+
+    private void FixedUpdate()
+    {
+        if(GameplayController.instance.gamePlaying)
+        {
+            if(rollLeft)
+            {
+                myBody.velocity = new Vector3(-speed, Physics.gravity.y, 0f);
+            }
+            else
+            {
+                myBody.velocity = new Vector3(0f, Physics.gravity.y, speed);
+            }
+        }
+    }
+
+    void CheckBallOutOfBounds()
+    {
+        if (GameplayController.instance.gamePlaying)
+        {
+            if (transform.position.y < -4)
+            {
+                GameplayController.instance.gamePlaying = false;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!GameplayController.instance.gamePlaying)
+            {
+                GameplayController.instance.gamePlaying = true;
+                GameplayController.instance.ActiveTileSpawner();
+            }
+        }
+
+        if (GameplayController.instance.gamePlaying)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rollLeft = !rollLeft;
+            }
+        }
+
+    }
+
+
+} // Ball Script class
