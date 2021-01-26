60e83c1be3e89c8980d32206d0a17e8642c237dd
Jose <joey_manuel@outlook.com>
2021-01-26 17:54:29 -0500

    6.Gem Mechanim
    created the following files:
    - GemHover.cs
    - GemRotate.cs
    - GemScript.cs
    - SparkleScript.cs
    
    The game is ready for merging

---
+++ Zig-Zag Game/Assets/Scripts/Gem Scripts/SparkleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SparkleScript : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(DeactiveAferTime());
+    }
+
+    IEnumerator DeactiveAferTime()
+    {
+        yield return new WaitForSeconds(1.5f);
+        gameObject.SetActive(false);
+    }
+}
